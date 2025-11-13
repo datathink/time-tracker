73d7724810c2769c254e17cb2e16ecc4c823d631
ugohuche <ucugooh@gmail.com>
2025-11-13 18:59:55 +0100

    Revert "Ugohu/dt 13 create profile update page (#12)"
    This reverts commit 806cd95291fb111820493a1c90882accf33a2565.

--- src/lib/schemas/user.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-// Validation schemas
-export const usernameSchema = z.object({
-    fullName: z.string().min(3, "Full name is required"),
-});
-
-export type UsernameUpdateData = z.infer<typeof usernameSchema>;
