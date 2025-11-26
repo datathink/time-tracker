a41f60a3dc5320162487af9414155299f77ae157
Meshack Amadi <meshackamadi@gmail.com>
2025-11-26 03:24:01 +0100

    WIP: start rate renaming migration - update client schema

---
+++ prisma/migrations/20251125114108_updatedmigration/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `payoutRate` on the `ProjectMember` table. All the data in the column will be lost.
+  - Added the required column `payoutRate` to the `ProjectMember` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "ProjectMember" DROP COLUMN "payoutRate",
+ADD COLUMN     "payoutRate" DECIMAL(10,2) NOT NULL;
