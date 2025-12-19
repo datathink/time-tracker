1dc8b99864feaf38fe2235d30eb9038e7694bf03
Ethan Glenn <ethan+git@eglenn.dev>
2025-12-18 23:21:52 -0700

    Updated color scheme and updated landing page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,21 @@
 import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md">
-        <div className="text-center mb-8">
-          <h1 className="text-3xl font-bold text-gray-900">Time Tracker</h1>
-          <p className="mt-2 text-sm text-gray-600">
-            Simple and efficient time tracking
-          </p>
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="w-full max-w-md">
+                <div className="text-center mb-8">
+                    <h1 className=" flex gap-2 items-center justify-center text-3xl text-gray-900">
+                        <span className="font-bold">Time</span>
+                        <span>by DataThink</span>
+                    </h1>
+                    <p className="mt-2 text-sm text-gray-600">
+                        Track hours, projects, and clients with ease, all in one
+                        place.
+                    </p>
+                </div>
+                {children}
+            </div>
         </div>
-        {children}
-      </div>
-    </div>
-  );
+    );
 }
